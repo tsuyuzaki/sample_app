5ac2b8e81a87131c1cf809ef5ebaa158205b43cc
tsuyuzaki <norihiko.tsuyuzaki@mazrica.com>
2019-12-03 22:08:39 +0900

    Modified config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = 'pacific-fjord-41763.herokuapp.com'
+  host = 'https://pacific-fjord-41763.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
